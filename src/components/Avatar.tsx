b2d14724459e4ce791a535e63bd6441f0b8c9fb5
Nadi Duno <nadiduno@gmail.com>
2023-02-26 13:27:15 -0300

    Update Avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,7 +5,7 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean
 }
 export function Avatar({
-  src = 'https://github.com/nadiduno.png',
+  src = 'https://github.com/nadiduno/post/blob/main/src/assets/avatarcomicsneon.jpg?raw=true',
   hasBorder = true,
   ...props
 }: AvatarProps) {
